6e3d663c27a760c03b0dabbbbe0cff40326a27df
Sahil Naik <nsahil992@gmail.com>
2023-11-08 22:19:30 +0530

    file modified

--- code.jl
+++ code.jl
@@ -1,2 +1,9 @@
 using Statistics
 using Flux.Tracker:
+
+inputs = [0.2, -0.3, 0.5, 1, -0.9]
+outputs = [-0.2, 0.3, -0.5, -1, 0.9]
+weight = 0.4
+
+predict(inputs, weight) = inputs .* weight
+loss(inputs, outputs, weight)
