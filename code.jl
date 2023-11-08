8b7606921ef435d60e6fb37871b055c53ac49a95
Sahil Naik <nsahil992@gmail.com>
2023-11-08 22:20:40 +0530

    file modified

--- code.jl
+++ code.jl
@@ -6,4 +6,5 @@ outputs = [-0.2, 0.3, -0.5, -1, 0.9]
 weight = 0.4
 
 predict(inputs, weight) = inputs .* weight
-loss(inputs, outputs, weight)
+loss(inputs, outputs, weight) = mean((outputs - (inputs .* weight)) .^2)
+
